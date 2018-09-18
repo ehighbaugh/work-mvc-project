6caa4d699adeb66bb82ee21df9f7a9f3d75612ff
erin.highbaugh <erin.highbaugh@waystar.com>
2018-09-18 07:08:18 -0400

    Initial commit of completed project

---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"},
+            });
+
+            // create controller and make page size 3 items
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2.ToString()).Model;
+
+            // assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext,
+            ModelBindingContext bindingContext)
+        {
+            // get the cart from session
+
+            Cart cart = null;
+            if (controllerContext.HttpContext.Session != null )
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+            // create the cart if one does not exist
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null )
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+            
+            return cart;
+        }
+    }
+}
